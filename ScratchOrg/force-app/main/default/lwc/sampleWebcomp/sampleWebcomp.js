575a0da516fdac8656fd66580c4af33b3080db56
LathabG <109475000+LathabG@users.noreply.github.com>
2022-08-22 14:19:17 -0500

    Just modified the file

--- ScratchOrg/force-app/main/default/lwc/sampleWebcomp/sampleWebcomp.js
+++ ScratchOrg/force-app/main/default/lwc/sampleWebcomp/sampleWebcomp.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class SampleWebcomp extends LightningElement {}
+export default class SampleWebcomp extends LightningElement {
+    name;
+}
